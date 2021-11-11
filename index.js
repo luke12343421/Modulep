4d796567203cec67c93ddd560b2e804aa2f3cb91
luke12343421 <testestest521@gmail.com>
2021-11-11 17:05:04 -0500

    main

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 
 
 exports.printMsg = function(_a,_c) {
-
-
-
+  
+    if (_a === undefined){
+        throw new Error("Undefined")   
+    }else if (_c ===undefined){
+        throw new Error("Undefined")   
+    }
 }
