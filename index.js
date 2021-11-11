ad20b926ba43635131c7a376ad65aa5c1687c4da
luke12343421 <testestest521@gmail.com>
2021-11-11 17:06:49 -0500

    main

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-
-
+const test = require('prompt-sync')({ sigint: true });
 
 
 exports.printMsg = function(_a,_c) {
   
     if (_a === undefined){
-        throw new Error("Undefined")   
-    }else if (_c ===undefined){
-        throw new Error("Undefined")   
+        throw new Error("Value: Undefined")   
+    }else if (_c === undefined){
+        throw new Error("Value: Undefined")   
     }
+    
+
 }
